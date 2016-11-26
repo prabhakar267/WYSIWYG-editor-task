9faf23c1b3b644285565985605b991da71a16759
Mayank Badola <badola21295@gmail.com>
2016-11-26 11:52:07 +0530

    Tooltip redesign

--- editor.js
+++ editor.js
@@ -25,9 +25,9 @@ function divideParagraph(el) {
   let leftPart = referenceString.slice(0, caretPosition);
   let rightPart = referenceString.slice(caretPosition);
 
-  if (leftPart != 0 && rightPart != 0) {
+  if (leftPart != 0) {
     $(el).text(leftPart);
-    $(el).after('<p contenteditable="true" placeholder="Write here!">' + rightPart + '</p>');
+    $(el).after('<p contenteditable="true" placeholder="Here. Take this space to write ...">' + rightPart + '</p>');
     $(el).next().focus();
   }
 }
@@ -36,17 +36,7 @@ dragula([document.querySelector('#editor')]);
 
 $('body').on('keydown', '#editor>p', e => {
   let domNode = e.target;
-  if (e.keyCode == 8) {
-    if ($('#editor>p').length > 1) {
-      if ($(domNode).html().length == 1 && domNode == document.querySelector('#editor>p:first-child')) {
-        $(domNode).remove();
-        $('#editor>p:first-child').focus();
-      } else if ($(domNode).html().length == 0) {
-        placeCaretAtEnd($(domNode).prev().get(0))
-        $(domNode).remove();
-      }
-    }
-  } else if (e.keyCode == 13) {
+  if (e.keyCode == 13) {
     divideParagraph(domNode);
     return false;
   }
@@ -58,14 +48,14 @@ $('body').on('keydown', '#editor>p:last-child', e => {
     let caretPosition = window.getSelection().anchorOffset;
     let paralength = $(domNode).html().length;
     if (paralength != 0 && caretPosition == paralength) {
-      $('#editor').append('<p contenteditable="true" placeholder="Write here!"></p>');
+      $('#editor').append('<p contenteditable="true" placeholder="Here. Take this space to write ..."></p>');
       $('#editor>p:last-child').focus();
     }
     return false;
   }
 });
 
-$('body').on('focusout', '#editor>p:last-child', e => {
+$('body').on('focusout', '#editor>p', e => {
   let domNode = e.target;
   if ($(domNode).text().length == 0 && $('#editor>p').length > 1) {
     $(domNode).remove();
@@ -76,11 +66,10 @@ $('body').on('dblclick', '#editor>p', e => {
   $('#style-tooltip').remove();
   let selectedItemRange = window.getSelection().getRangeAt(0);
   let stringInRange = selectedItemRange.toString();
-  console.log(e.clientX);
-  console.log(e.clientY);
+  let locationBounds = selectedItemRange.getBoundingClientRect();
   $('body').append(
-    '<div id="style-tooltip" style="left: ' + e.clientX + 'px; top: ' + (e.clientY - 50) + 'px">\
-      <span data-active="true" data-value="bold">B</span><span data-value="italics">I</span><span data-value="red">R</span>\
+    '<div id="style-tooltip" style="left: ' + locationBounds.left + 'px; top: ' + (locationBounds.top - 40) + 'px">\
+      <span data-active="true" data-value="bold">B</span><span data-active="false" data-value="italics">I</span><span data-active="false" data-value="red">R</span>\
     </div>'
   );
   // let childComponents = '<em>' + stringInRange + '</em>';
